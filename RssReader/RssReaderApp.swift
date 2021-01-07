ad750462e03148ef91b1ff8e61257496f3690cd7
Tomoyuki Murakami <tomoyukim@outlook.com>
2021-01-07 21:38:30 +0900

    Initial Commit

---
+++ RssReader/RssReaderApp.swift
@@ -0,0 +1,20 @@
+//
+//  RssReaderApp.swift
+//  RssReader
+//
+//  Created by Tomoyuki Murakami on 2021/01/07.
+//
+
+import SwiftUI
+
+@main
+struct RssReaderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
